3c32018f34798ad4650e40863a4448ef74cbc01c
xiaohan <xiaohan2012@gmail.com>
2013-06-18 13:46:27 +0800

    done

--- ProcedureA.m
+++ ProcedureA.m
@@ -1,56 +1,56 @@
-function K = ProcedureA(T, N, gamma, theta, r, C, D, P, s)
-    K = zeros(N, 1);
-    StepOneVals = 1 / floor(T / gamma);
-    StepOnePredicates = theta / T <= 1;
+function K = ProcedureA(T, N, gamma, theta, r, C, D, P, s, m)
+    K = zeros(1, N);
+    StepOneVals = 1 ./ floor(T ./ gamma);
+    StepOnePredicates = (theta / T <= 1);
     K(StepOnePredicates) = StepOneVals(StepOnePredicates);
     
-    k_upperline = zeros(N, 1);
+    k_upperline = zeros(1, N);
     for i = 1:N
-        if StepOnePredicates(i) == 1 %if case one, ignore
-            continue
+      if StepOnePredicates(i) == 1 %if case one, ignore
+        continue
+      end
+      
+      %calculating caret k 
+      k_caret = 1;
+      while true
+        value = 2 * s(i) * P(i) / (T^2 * r * C(i) * D(i) * (2*P(i) - D(i)));
+        if value > k_caret * (k_caret - 1) && value <= k_caret * (k_caret + 1)
+          break
         end
-        
-        %calculating caret k 
-        k_caret = 1;
-        while true
-            value = 2 * s(i) * P(i) / (T^2 * r * C(i) * D(i) * (2*P(i) - D(i)));
-            if value > k_caret * (k_caret - 1) && value <= k_caret * (k_caret + 1)
-                break
-            end
-            k_caret = k_caret + 1;
-        end
-        
-        %calculating upperline k 
-        k_upperline(i) = min(k_caret, floor(theta(i) / t));
-        
-        %calculating set G
-        l = 1
-        LowerBound = k_upperline;
-        UpperBound = floor(theta(i) / T);
-        ys = [];
-        while true
-            y = l * P(i) / (P(i) - D(i));
-            if y > LowerBound
-                if y <= UpperBound
-                    ys = [ys y];
-                else
-                    break;
-                end
-            end            
-        end
-        G = [ys k_upperline];
-        
-        %calculating set caret G
-        if gamma(i) / T >= 1
-            G_caret = G;
-        else
-            G_caret = [G 1 / floor(T/gamma(i))];
-        end
-        
-        %find the element in G that minimize 'f'
-        f_k = s(i) / (T .* G_caret) + r * T .* G_caret * C(i) * D(i) * (2 - D(i) / P(i)); %r * T * C(i) * D(i) * m(i) can be omitted
-        [minVal, minIndex] = min(f_k); %find the min
-        
-        K(i) = G_caret(minIndex);
+        k_caret = k_caret + 1;
+      end
+      
+      %calculating upperline k 
+      k_upperline(i) = min(k_caret, floor(theta(i) / T));
+      
+      %calculating set G
+      l = 1;
+      LowerBound = k_upperline;
+      UpperBound = floor(theta(i) / T);
+      ys = [];
+      while true
+        y = l * P(i) / (P(i) - D(i));
+        if y > LowerBound
+          if y <= UpperBound
+            ys = [ys y];
+          else
+            break;
+          end
+        end            
+      end
+      G = [ys k_upperline];
+
+      %calculating set caret G
+      if gamma(i) / T >= 1
+        G_caret = G;
+      else
+        G_caret = [G 1 / floor(T/gamma(i))];
+      end
+      
+      %find the element in G that minimize 'f'
+      f_k = s(i) ./ (T .* G_caret) + r * T .* G_caret * C(i) * D(i) * (2 - D(i) / P(i)) - r * T * C(i) * D(i) * m(i);
+      [minVal, minIndex] = min(f_k); %find the min
+      
+      K(i) = G_caret(minIndex);
     end
 end
--- main.m
+++ main.m
@@ -1,30 +1,53 @@
-N = 10;
+%éœ€è¦ä¿®æ”¹çš„æ•°æ® Start
 
-%global D A C_ex r_ex T0 T kT S s P C beta gamma theta;
+N = 5;
 
-D = ones(N, 1); %annual demand of buyer i with the item i 
-A = ones(N, 1); %ordering cost per order of buyer i 
-C_ex = ones(N, 1); %the unit purchase cost of buyer i 
-r_ex = ones(N, 1); %the annual inventory holding cost per dollar invested in stocks of buyer i 
-T0 = sqrt(2 .* A ./ (r_ex .* C_ex .* D)); %the optimal order interval for buyer 
-T = 1; %the vendor setup time interval 
-kT = ones(N, 1); %buyer i purchasing time interval after negotiation    k¡Ê(1, 1/2, 1/3¡­¡­)¡È(1,2,3¡­¡­) 
-S = 1; %the major setup cost for the vendor 
-s = ones(N, 1); %the minor setup cost when item i is produced 
-P = 1; %the production rate of the vendor 
-C = 1; %vendor¡¯s unit production cost
+D = [200 200 200 100 150]; %annual demand of buyer i with the item i 
+A = [20 20 25 30 15]; %ordering cost per order of buyer i 
+C_caret = [25 15 25 30 30]; %the unit purchase cost of buyer i 
+r = 0.2;
+r_caret = 0.2 .* ones(1, N); %the annual inventory holding cost per dollar invested in stocks of buyer i 
+T0 = sqrt(2 .* A ./ (r_caret .* C_caret .* D)); %the optimal order interval for buyer 
+S = 300; %the major setup cost for the vendor 
+s = [100 80 100 90 150]; %the minor setup cost when item i is produced 
+P = [320 300 250 300 300]; %the production rate of the vendor 
+C = [20 10 15 25 20]; %vendor's unit production cost
 
-beta = ones(N, 1);
+
+%éœ€è¦ä¿®æ”¹çš„æ•°æ® End
+
+
+beta = ones(1, N) .* 1.1;
 gamma = T0 .* (beta - sqrt(beta .^ 2 - 1));
 theta = T0 .* (beta + sqrt(beta .^ 2 - 1));
 
-%heuristic part
-k = ones(N, 1);
-a = max(gamma ./ k);
-a_caret = min(theta ./ k);
 
-if a < a_caret
-    T_caret = sqrt(2 * (S + sum(s/max(k, 1))) / ( r * sum(max(1, k) .* C .* D (1 + min(1, k) - D ./ P - 2 * m ./ k))));
+%heuristic part
+k = ones(1, N); %initialize all ks to 1
+
+while 1
+  a = max(gamma ./ k);
+  a_caret = min(theta ./ k);
+  
+  m = floor(k .* (1 - D ./ P));
+  
+  %compute T value
+  if a < a_caret
+    T_caret = sqrt(2 .* (S + sum(s ./ max(k, 1))) / ( r .* sum(max(1, k) .* C .* D .* (1 + min(1, k) - D ./ P - 2 .* m ./ k))));
+    T = min(max(a, T_caret), a_caret);
+  else
+    T = (a + a_caret) / 2;
+  end
+
+  current_k = ProcedureA(T, N, gamma, theta, r, C, D, P, s, m);
+
+  if all(current_k == k) %stop iteration
+    KT = k * T
+    T
+    break
+  else
+    k = current_k;%continue iteration
+  end
+    
 end
-k = ProcedureA(T, N, gamma, theta, r, C, D, P, s)
 
